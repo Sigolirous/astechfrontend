8bb67a0d1ab589a2c212447304a4d5f55339bb62
sigolirous <augustoasigolo@gmail.com>
2019-12-09 15:51:00 -0300

    add the recaptcha

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,8 @@ import productsIcon from '../images/laptop.svg'
 import logo from '../images/logoBW.svg'
 import coloredLogo from '../images/logo.svg'
 import API from '../services/api'
-import Recaptcha from 'react-recaptcha'
+const Recaptcha = require('react-recaptcha');
+
 
 export default class Contact extends React.Component {
     constructor(props) {
@@ -18,20 +19,32 @@ export default class Contact extends React.Component {
                 order:"No",
                 projectId: "No order",
                 message:"",
+                autenticated:false,
+                captchaInstanse:undefined
         };
         this.submitEmail = (e) => {
-            API.post('/sendMail', this.state).then((res)=>{
-                if(res.data.error){
-                    alert("I'm sorry, an error has occured while trying to send this email. Come back later or send a diret amail to contato.augustosigolo@gmail.com")
-                }else{
-                    this.setState({
-                        from:"",
-                        subject:"",
-                        message:""
-                    })
-                    alert('Thanks, I will be returning you email soon!')
-                }
-            })
+            if(this.state.autenticated === true){
+                API.post('/sendMail', this.state).then((res)=>{
+                    if(res.data.error){
+                        alert("I'm sorry, an error has occured while trying to send this email. Come back later or send a diret amail to contato.augustosigolo@gmail.com")
+                        this.captchaInstanse.reset();
+                    }else if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(this.state.from) === false){
+                        alert('Input a valid email so I can return you')
+                    }else{
+                        this.setState({
+                            from:"",
+                            subject:"",
+                            message:"",
+                            autenticated:false,
+                        })
+                        this.captchaInstanse.reset();
+                        alert('Thanks, I will be returning you email soon!')
+                    }
+                    
+                })
+            }else{
+                alert('Verify if you´re a human')
+            }
             e.preventDefault();
         };
         this.onChange = (e) => {
@@ -48,8 +61,13 @@ export default class Contact extends React.Component {
                 projectId: params.id})
             }
         }
-        this.callbackRecaptcha = () => {
-            console.log('Recaptcha loaded')
+        this.callback = function () {
+            console.log('Done!!!!');
+          };
+        this.verifyCallback =(res) =>{
+            if(res){
+                this.setState({autenticated: true})
+            }
         }
       }
       componentDidMount(){
@@ -83,13 +101,22 @@ export default class Contact extends React.Component {
                <div className="emailFormContainer">
                     <div className="emailForm">
                         <form onSubmit={this.submitEmail}>
-                            <input id="from" type="email" placeholder="Your Best Email" value={this.state.from} onChange={this.onChange}/>
-                            <select id="subject" value={this.state.subject} onChange={this.onChange}>
+                            <input id="from" type="email" placeholder="Your Best Email" required value={this.state.from} onChange={this.onChange}/>
+                            <select id="subject" required value={this.state.subject} onChange={this.onChange}>
                                 <option value="order">I want to order a project!</option>
                                 <option value="problem">There is a problem with a project I bought from you!</option>
                                 <option value="chat">I just want to chat!</option>
                             </select>
-                            <textarea id="message" placeholder="What you want to tell me about it?" value={this.state.message} onChange={this.onChange}></textarea><br/>
+                            <textarea id="message" placeholder="What you want to tell me about it?" required value={this.state.message} onChange={this.onChange}></textarea><br/>
+                            <div className="captcha">
+                                <Recaptcha
+                                    ref={e => this.captchaInstanse = e}
+                                    sitekey="6LeN3cYUAAAAAKeaOUYjaOCcoLgrJAT6YnKqLUQm"
+                                    render="explicit"
+                                    onloadCallback={this.callback}
+                                    verifyCallback={this.verifyCallback}
+                                />
+                            </div>
                             <input type="submit" onClick={this.submitEmail} value="Submit"/>
                         </form>
                     </div>
